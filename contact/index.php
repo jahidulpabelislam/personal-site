e8e325d3b92b58b872004576f419bbdf38753d7f
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2016-10-27 11:21:24 +0100

    Instant Contact Form Feedback.
    Contact form has more instant feedback when submitting instead of
    reloading the page.
    Edit of form JavaScript.
    Separate PHP file for validating form inputs and sending mail.
    Edit of Contact Form Page.

--- contact/index.php
+++ contact/index.php
@@ -1,155 +1,89 @@
 <?php
-    $title = "Contact";
-    $keywords = "";
-    $description = "Contact Or Find Contact Information for Jahidul Pabel Islam, a Web and Software Developer in Bognor Regis, West Sussex Down by the South Coast of England.";
-    $description2 = "Send Your Feedback &amp; Enquires My Way";
-    include '../inc/header.php';
+$title = "Contact";
+$keywords = "";
+$description = "Contact Or Find Contact Information for Jahidul Pabel Islam, a Web and Software Developer in Bognor Regis, West Sussex Down by the South Coast of England.";
+$description2 = "Send Your Feedback &amp; Enquires My Way";
+include '../inc/header.php';
 ?>
-                <script src="/lib/formValidator.js"></script>
 
-                <p id="formConformation">
-                    <?php
+    <p class="article">Please feel free to contact me. All my details are below.</p>
 
-                    //checks if user clicked submit
-                    if (isset($_POST['submit'])) {
+    <div class="article">
+        <h3>Form</h3>
+        <p>Use this contact form to give me a message quickly.</p>
+        <p><span class="required">*</span> indicates a required field</p>
 
-                        $fail = true;
+        <!--validate using JavaScript on submit-->
+        <form id="contactForm" name="contactForm" method="post" onsubmit="return window.portfolio.form.validateForm()">
 
-                        //the address to send mail to
-                        $to = 'jahidul.pabel.islam@hotmail.com';
+            <label for="email">Email Address <span class="required">*</span></label>
+            <p id="emailFeedback"></p>
+            <!--validate using JavaScript on input-->
+            <input type="email" name="email" placeholder="e.g. joe@example.com" required tabindex="1"
+                   oninput="window.portfolio.form.validateEmail(this.value);" class="input" id="email"
+                   title="Email Address">
 
-                        //the subject user provided for message
-                        $subject = $_POST['subject'];
+            <label for="subject">Subject</label>
+            <input type="text" name="subject" placeholder="e.g. Site Feedback" tabindex="2" class="input" id="subject"
+                   title="Subject of Message">
 
-                        //the message user wrote
-                        $message = $_POST['message'];
+            <label for="message">Message <span class="required">*</span></label>
+            <p id="messageFeedback"></p>
+            <!--validate using JavaScript on input-->
+            <textarea name="message" placeholder="e.g. Your site could do with more colour." required tabindex="3"
+                      oninput="window.portfolio.form.validateMessage(this.value);" class="input" id="message"
+                      title="The Message"></textarea>
 
-                        //the email user provided to reply to
-                        $email = trim($_POST['email']);
+            <p id="formFeedback"></p>
+            <p id="emailFeedback2"></p>
+            <p id="messageFeedback2"></p>
 
-                        //creates the header for sending email
-                        $headers = 'From: contact@jahidulpabelislam.net16.net' . "\r\n" . 'Reply-To: ' . $email;
+            <button name="submit" type="submit" class="btn btn-lg btn-primary btn-block">Submit</button>
 
-                        //checks if message is not empty
-                        if (trim($message) != "") {
+        </form>
+    </div>
 
-                            //checks if email is not empty
-                            if ($email != "") {
+    <div class="article">
+        <h3>Email</h3>
+        <p>Use the following email address to drop me a email.</p>
+        <a href="mailto:jahidul.pabel.islam@hotmail.com" target="_blank"><img src="../images/email.svg"
+                                                                              alt="Email me"
+                                                                              class="socialMedia email"></a>
+        <a href="mailto:jahidul.pabel.islam@hotmail.com" target="_blank"> jahidul.pabel.islam@hotmail.com</a>
+    </div>
 
-                                //checks if email provided is valid using REGEX
-                                if (preg_match("/\b[\w._-]+@[\w-]+.[\w]{2,}\b/im", $email)) {
+    <div class="article">
+        <h3>Linkedin</h3>
+        <p>View my profile on Linkedin.</p>
+        <a href="http://uk.linkedin.com/in/jahidulpabelislam" target="_blank"><img src="../images/linkedin.svg"
+                                                                                   alt="Find me on Linkedin /jahidulpabelislam"
+                                                                                   class="socialMedia linkedin"></a>
+        <a href="http://uk.linkedin.com/in/jahidulpabelislam" target="_blank">/jahidulpabelislam</a>
+    </div>
 
-                                    //if user didn't provide subject create a default one
-                                    if (trim($subject) == "") $subject = 'ePortfolio Contact Form';
+    <div class="article">
+        <h3>GitHub</h3>
+        <p>Look at all my code for projects i've done and follow future projects.</p>
+        <a href="https://github.com/jahidulpabelislam" target="_blank"><img src="../images/github.svg"
+                                                                            alt="Find me on GitHub /jahidulpabelislam"
+                                                                            class="socialMedia github"></a>
+        <a href="https://github.com/jahidulpabelislam" target="_blank">/jahidulpabelislam</a>
+    </div>
 
-                                    //try to send email
-                                    if (mail($to, $subject, $message, $headers)) {
-                                        echo "<span class='formFeedback correct'>Your message has been sent.</span>";
-                                        $fail = false;
-                                    }
+    <div class="article action">
+        <div>
+            <a class="btn btn-primary" href="/projects/">View My Work</a>
+        </div>
 
-                                    //if not sent give message
-                                    else echo '<span class="formFeedback error">Something went wrong, please try again.</span>';
-                                }
-                            }
-                        }
-                    }
+        <div>
+            <a class="btn btn-warning" href="/about/">Learn About Me</a>
+        </div>
+    </div>
 
-                    ?>
-                </p>
+    <script src="/lib/helperFunctions.js"></script>
+    <script src="/lib/xhr.js"></script>
+    <script src="/lib/formValidator.js"></script>
 
-                <p class="article">Please feel free to contact me. All my details are below.</p>
-
-                <div class="article">
-                    <h3>Form</h3>
-                    <p>Use this contact form to give me a message quickly.</p>
-                    <p><span class="required">*</span> indicates a required field</p>
-                    
-                    <!--validate using JavaScript on submit-->
-                    <form id="contactForm" name="contactForm" method="post" onsubmit="return window.portfolio.form.validateForm(this)">
-
-                        <label for="email">Email Address <span class="required">*</span></label>
-                        <p id="emailFeedback">
-
-                            <?php
-
-                            if (isset($email)) {
-                                //checks if email is empty
-                                if ($email == "") {
-                                    echo "<span class='formFeedback error'>Email Address isn't provided.</span>";
-
-                                } else if (!preg_match("/\b[\w._-]+@[\w-]+.[\w]{2,}\b/im", $email)) { //checks if email isn't valid
-                                    echo "<span class='formFeedback error'>Email Address isn't valid.</span>";
-                                }
-                            }
-
-                            ?>
-
-                        </p>
-
-                        <input type="email" name="email" placeholder="e.g. joe@example.com" required tabindex="1" oninput="window.portfolio.form.validateEmail(this.value);" class="input" id="email"  title="Email Address" value="<?php if ($fail) echo $email ?>">
-                        <!--validate using JavaScript on input-->
-
-                        <label for="subject">Subject</label>
-                        <input type="text" name="subject" placeholder="e.g. Site Feedback" tabindex="2" class="input" id="subject" title="Subject of Message" value="<?php if ($fail) echo $subject ?>">
-
-                        <label for="message">Message <span class="required">*</span></label>
-
-                        <p id="messageFeedback">
-
-                            <?php
-                                //checks if message is empty
-                                if (isset($message) && trim($message) == "") echo "<span class='formFeedback error'>Message isn't provided.</span>";
-                            ?>
-
-                        </p>
-
-                        <textarea name="message" placeholder="e.g. Your site could do with more colour." required tabindex="3" oninput="window.portfolio.form.validateMessage(this.value);" class="input" id="message" title="The Message" ><?php if ($fail) echo $message ?></textarea>
-                        <!--validate using JavaScript on input-->
-                        
-                        <p id="formFeedback"></p>
-                        <p id="emailFeedback2"> </p>
-
-                        <p id="messageFeedback2"></p>
-
-                        <button name="submit" type="submit" class="btn btn-lg btn-primary btn-block">Submit</button>
-
-                    </form>
-                </div>
-
-                <div class="article">
-                    <h3>Email</h3>
-                    <p>Use the following email address to drop me a email.</p>
-                    <a href="mailto:jahidul.pabel.islam@hotmail.com" target="_blank"><img src="../images/email.svg"
-                                                                          alt="Email me" class="socialMedia email"></a>
-                    <a href="mailto:jahidul.pabel.islam@hotmail.com" target="_blank"> jahidul.pabel.islam@hotmail.com</a>
-                </div>
-
-                <div class="article">
-                    <h3>Linkedin</h3>
-                    <p>View my profile on Linkedin.</p>
-                    <a href="http://uk.linkedin.com/in/jahidulpabelislam" target="_blank"><img src="../images/linkedin.svg"
-                                                                           alt="Find me on Linkedin /jahidulpabelislam" class="socialMedia linkedin"></a>
-                    <a href="http://uk.linkedin.com/in/jahidulpabelislam" target="_blank">/jahidulpabelislam</a>
-                </div>
-
-                <div class="article">
-                    <h3>GitHub</h3>
-                    <p>Look at all my code for projects i've done and follow future projects.</p>
-                    <a href="https://github.com/jahidulpabelislam" target="_blank"><img src="../images/github.svg"
-                                                                                alt="Find me on GitHub /jahidulpabelislam" class="socialMedia github"></a>
-                    <a href="https://github.com/jahidulpabelislam" target="_blank">/jahidulpabelislam</a>
-                </div>
-
-                <div class="article action">
-                    <div>
-                        <a class="btn btn-primary" href="/projects/">View My Work</a>
-                    </div>
-            
-                    <div>
-                        <a class="btn btn-warning" href="/about/">Learn About Me</a>
-                    </div>
-                </div>
 <?php
 include '../inc/footer.html';
 ?>
---
+++ contact/sendMessage.php
@@ -0,0 +1,84 @@
+<?php
+
+$data = array();
+foreach ($_REQUEST as $key => $value) {
+    $data[$key] = stripslashes(strip_tags(urldecode(filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING))));
+}
+
+//the email user provided to reply to
+$emailAddress = $data["emailAddress"];
+
+$message = $data["message"];
+
+$subject = $data["subject"];
+
+//the address to send mail to
+$to = 'jahidul.pabel.islam@hotmail.com';
+
+$meta["ok"] = true;
+
+//checks if message is not empty
+if (trim($message) === "") {
+    $meta["ok"] = false;
+    $meta["messageFeedback"] = "Message isn't provided.";
+}
+
+//checks if email is not empty
+if (trim($emailAddress) === "") {
+    $meta["ok"] = false;
+    $meta["emailAddressFeedback"] = "Email Address isn't provided.";
+} //checks if email provided is valid using REGEX
+else if (!preg_match("/\b[\w._-]+@[\w-]+.[\w]{2,}\b/im", $emailAddress)) {
+    $meta["ok"] = false;
+    $meta["emailAddressFeedback"] = "Email Address isn't valid.";
+}
+
+//checks if message is not empty
+if ($meta["ok"]) {
+
+    //if user didn't provide subject create a default one
+    if (trim($subject) == "") $subject = 'ePortfolio Contact Form';
+
+    //creates the header for sending email
+    $headers = 'From: contact@jahidulpabelislam.net16.net' . "\r\n" . 'Reply-To: ' . $emailAddress;
+
+    //try to send email
+    if (mail($to, $subject, $message, $headers)) {
+
+        //echo "<span class='formFeedback correct'>Your message has been sent.</span>";
+        $meta["feedback"] = "Your message has been sent.";
+    } //if not sent give message
+    else {
+        //echo '<span class="formFeedback error"></span>';
+        $meta["ok"] = false;
+        $meta["feedback"] = "Something went wrong, please try again.";
+    }
+}
+
+//check if requested to send json
+$json = (stripos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false);
+
+//check is everything was ok
+if (isset($meta["ok"]) && $meta["ok"] !== false) {
+    $status = isset($meta["status"]) ? $meta["status"] : 200;
+    $message = isset($meta["message"]) ? $meta["message"] : "OK";
+} else {
+    $status = isset($meta["status"]) ? $meta["status"] : 500;
+    $message = isset($meta["message"]) ? $meta["message"] : "Internal Server Error";
+}
+$meta["data"] = $data;
+$meta["status"] = $status;
+$meta["message"] = $message;
+
+header("HTTP/1.1 $status $message");
+
+//send the results, send by json if json was requested
+if ($json) {
+    header("Content-Type: application/json");
+    echo json_encode($meta);
+} //else send by plain text
+else {
+    header("Content-Type: text/plain");
+    echo("results: ");
+    var_dump($meta);
+}
