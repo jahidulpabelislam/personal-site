560e44195340194cebbae32713ddf20fe98b0f28
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-05-16 12:10:39 +0100

    Display projects (Admin Page).

--- admin/lib/js/ui.js
+++ admin/lib/js/ui.js
@@ -1,7 +1,38 @@
 angular.module('projectsAdmin', [])
     .controller('projectsAdminController', function($scope, $http) {
 
-        $scope.userFormFeedback = "";
+        $scope.projects = [];
+
+        $scope.userFormFeedback = $scope.selectProjectFeedback = "";
+
+        var gotProjects = function(result) {
+            $("#projectFormContainer").hide();
+            $("#selectProjectContainer").show();
+            
+            //check if data doesn't exist check there's no feedback
+            if (result.data.meta.ok && result.data.rows.length <= 0 && !result.data.meta.feedback) {
+
+                //assume there's no error and no projects to show
+                $scope.selectProjectFeedback = "Sorry, no Projects to show.";
+            } else if (result.data.rows.length > 0) {
+                $scope.projects = result.data.rows;
+            }
+
+            delayExpand();
+        };
+
+        $scope.getProjectList = function() {
+            dragNDropStop();
+
+            //Sends a object with necessary data to XHR
+            $http({
+                url: "/admin/api/1/projects",
+                method: "GET",
+                params: {page: $("#currentPage").val() || 1}
+            }).then(gotProjects, function(result) {
+                $scope.userFormFeedback = result.data.meta.feedback || "Error logging you in."
+            });
+        };
 
         //after user has attempted to log in
         var loggedIn = function(result) {
@@ -16,7 +47,7 @@ angular.module('projectsAdmin', [])
                 //make the log in/sign up form not visible
                 $("#userFormDiv").hide();
 
-                // $scope.getProjectList();
+                $scope.getProjectList();
             }
             //check if feedback was provided or generic error message
             else {
