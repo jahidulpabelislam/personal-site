910dff4a76be58fbf7ff218f513bd4e23f8d2b1e
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-04-28 16:50:46 +0100

    Indent code

--- links/index.php
+++ links/index.php
@@ -21,67 +21,64 @@
     </head>
 
     <body>
+        <section class="social-links-page">
+            <div class="container">
 
-    <section class="social-links-page">
+                <div class="social-link-container">
+                    <a href="https://facebook.com/jahidulpabelislam/" target="_blank" class="social-link">
+                        <img src="/assets/images/facebook.svg?v=1" alt="Add Me facebook.com/jahidulpabelislam" class="social-link__img social-link__img--facebook">
+                        <p class="social-link__text social-link__text--facebook"> /jahidulpabelislam</p>
+                    </a>
+                </div>
 
-        <div class="container">
+                <div class="social-link-container">
+                    <a href="https://twitter.com/itsjahidulislam/" target="_blank" class="social-link">
+                        <img src="/assets/images/twitter.png?v=1" alt="Follow Me @ItsJahidulIslam" class="social-link__img social-link__img--twitter">
+                        <p class="social-link__text social-link__text--twitter"> @ItsJahidulIslam</p>
+                    </a>
+                </div>
 
-            <div class="social-link-container">
-                <a href="https://facebook.com/jahidulpabelislam/" target="_blank" class="social-link">
-                    <img src="/assets/images/facebook.svg?v=1" alt="Add Me facebook.com/jahidulpabelislam" class="social-link__img social-link__img--facebook">
-                    <p class="social-link__text social-link__text--facebook"> /jahidulpabelislam</p>
-                </a>
+                <div class="social-link-container">
+                    <a href="https://instagram.com/jahidulpabelislam/" target="_blank" class="social-link">
+                        <span class="social-link__img social-link__img--instagram"><i></i></span>
+                        <p class="social-link__text social-link__text--instagram"> @jahidulpabelislam</p>
+                    </a>
+                </div>
+
+                <div class="social-link-container social-link-container--snapchat">
+                    <!--Snapcode-->
+                    <a class="social-link" href="https://snapchat.com/add/jahidulpislam/" target="_blank">
+                        <object data="https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=jahidulpislam&amp;type=PNG" type="image/png" class="social-link__img social-link__img--snapchat"></object>
+                        <p class="social-link__text social-link__text--snapchat">jahidulpislam</p>
+                    </a>
+                </div>
+
+                <div class="social-link-container">
+                    <a href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" class="social-link">
+                        <img src="/assets/images/linkedin.svg?v=1" alt="Find me on Linkedin /jahidulpabelislam" class="social-link__img social-link__img--linkedin">
+                        <p class="social-link__text social-link__text--linkedin"> /jahidulpabelislam</p>
+                    </a>
+                </div>
+
+                <div class="social-link-container">
+                    <a href="https://github.com/jahidulpabelislam/" target="_blank" class="social-link">
+                        <img src="/assets/images/github.svg?v=1" alt="Find me on GitHub /jahidulpabelislam" class="social-link__img social-link__img--github">
+                        <p class="social-link__text social-link__text--github"> /jahidulpabelislam</p>
+                    </a>
+                </div>
+
+                <div class="social-link-container">
+                    <a href="https://instagram.com/jahidulcodes/" target="_blank" class="social-link">
+                        <span class="social-link__img social-link__img--instagram"><i></i></span>
+                        <p class="social-link__text social-link__text--instagram"> @jahidulcodes</p>
+                    </a>
+                </div>
+
             </div>
+        </section>
 
-            <div class="social-link-container">
-                <a href="https://twitter.com/itsjahidulislam/" target="_blank" class="social-link">
-                    <img src="/assets/images/twitter.png?v=1" alt="Follow Me @ItsJahidulIslam" class="social-link__img social-link__img--twitter">
-                    <p class="social-link__text social-link__text--twitter"> @ItsJahidulIslam</p>
-                </a>
-            </div>
-
-            <div class="social-link-container">
-                <a href="https://instagram.com/jahidulpabelislam/" target="_blank" class="social-link">
-                    <span class="social-link__img social-link__img--instagram"><i></i></span>
-                    <p class="social-link__text social-link__text--instagram"> @jahidulpabelislam</p>
-                </a>
-            </div>
-
-            <div class="social-link-container social-link-container--snapchat">
-                <!--Snapcode-->
-                <a class="social-link" href="https://snapchat.com/add/jahidulpislam/" target="_blank">
-                    <object data="https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=jahidulpislam&amp;type=PNG" type="image/png" class="social-link__img social-link__img--snapchat"></object>
-                    <p class="social-link__text social-link__text--snapchat">jahidulpislam</p>
-                </a>
-            </div>
-
-            <div class="social-link-container">
-                <a href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" class="social-link">
-                    <img src="/assets/images/linkedin.svg?v=1" alt="Find me on Linkedin /jahidulpabelislam" class="social-link__img social-link__img--linkedin">
-                    <p class="social-link__text social-link__text--linkedin"> /jahidulpabelislam</p>
-                </a>
-            </div>
-
-            <div class="social-link-container">
-                <a href="https://github.com/jahidulpabelislam/" target="_blank" class="social-link">
-                    <img src="/assets/images/github.svg?v=1" alt="Find me on GitHub /jahidulpabelislam" class="social-link__img social-link__img--github">
-                    <p class="social-link__text social-link__text--github"> /jahidulpabelislam</p>
-                </a>
-            </div>
-
-            <div class="social-link-container">
-                <a href="https://instagram.com/jahidulcodes/" target="_blank" class="social-link">
-                    <span class="social-link__img social-link__img--instagram"><i></i></span>
-                    <p class="social-link__text social-link__text--instagram"> @jahidulcodes</p>
-                </a>
-            </div>
-
-        </div>
-
-    </section>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="/assets/js/stickyFooter.js?v=1" type="text/javascript"></script>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="/assets/js/stickyFooter.js?v=1" type="text/javascript"></script>
     </body>
 </html>
