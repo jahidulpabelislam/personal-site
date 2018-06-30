8c3cca2f827c57ee1528232804d68bb2b481bdd8
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-06-30 14:42:36 +0100

    Update nav
    Add social links on left (desktop) & bottom (mobile).
    Remove my name next to logo.
    Remove home page link from links.
    Move logo to center on navigation.

--- inc/header.php
+++ inc/header.php
@@ -75,7 +75,7 @@
     <body>
 
         <nav class="nav nav--<?php echo $nav_tint; ?>">
-            <div class="container">
+            <div class="container nav__wrapper">
                 <div class="nav__mobile-header">
                     <button type="button" class="nav__links__toggle">
                         <span class="screen-reader-text">Toggle navigation</span>
@@ -84,14 +84,19 @@
                         <span class="menu-bar menu-bar--bottom"></span>
                     </button>
                     <a class="nav__brand nav__brand--logo-container" href="/<?php if ($page_title == "Home") echo "#";?>"><img class="nav__brand--logo <?php if ($page_title == "Home") echo "current";?>" src="/assets/images/logo.png" alt="Jahidul Pabel Islam Logo"></a>
-                    <a class="nav__brand nav__brand--text <?php if ($page_title == "Home") echo "current";?>" href="/">Jahidul Pabel Islam</a>
                 </div>
-                <div class="nav__links-container closed">
+                <div class="nav__links-container">
                     <ul class="nav__links">
-                        <li <?php if ($page_title == "Home") echo "class='active'"; ?> ><a href="/<?php if ($page_title == "Home") echo "#";?>" title="Link to Home Page">Home</a></li>
                         <li <?php if ($page_title == "Projects") echo "class='active'"; ?> ><a href="/projects/<?php if ($page_title == "Projects") echo "#";?>" title="Link to Projects Page">Projects</a></li>
                         <li <?php if ($page_title == "Contact") echo "class='active'"; ?> ><a href="/contact/<?php if ($page_title == "Contact") echo "#";?>" title="Link to Contact Page">Contact</a></li>
                         <li <?php if ($page_title == "About") echo "class='active'"; ?> ><a href="/about/<?php if ($page_title == "About") echo "#";?>" title="Link to About Page">About</a></li>
+                    </ul>
+                </div>
+                <div class="nav__social-links-wrapper">
+                    <ul class="nav__social-links">
+                        <li><a href="https://uk.linkedin.com/in/jahidulpabelislam" target="_blank" class="social-link"><img src="/assets/images/linkedin.svg" alt="Find me on Linkedin /jahidulpabelislam" class="social-link__img social-link__img--linkedin"></a></li>
+                        <li><a href="https://github.com/jahidulpabelislam" target="_blank" class="social-link"><img src="/assets/images/github.svg" alt="Find me on GitHub /jahidulpabelislam" class="social-link__img social-link__img--github"></a></li>
+                        <li><a href="https://www.instagram.com/jahidulcodes/" target="_blank" class="social-link"><span class="social-link__img social-link__img--instagram "><i></i></span></a></li>
                     </ul>
                 </div>
             </div>
