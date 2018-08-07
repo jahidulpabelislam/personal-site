c157aed4069ab73eaf9a1d828e07fdbf70d8e7d3
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-07 22:41:16 +0100

    Fix indents on gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -8,70 +8,70 @@ var sass = require('gulp-sass');
 
 // Concatenate & Minify JS
 var scripts = {
-    main: [
-        "assets/js/jpi/expandedSlideShow.js",
-        "assets/js/jpi/slideShow.js",
-        "assets/js/jpi/helpers.js",
-        "assets/js/jpi/ajax.js",
-        "assets/js/jpi/projects.js",
-        "assets/js/jpi/home.js",
-        "assets/js/jpi/form.js",
-        "assets/js/jpi/stickyFooter.js",
-        "assets/js/third-party/waypoint.min.js",
-        "assets/js/third-party/jquery.countTo.js",
-        "assets/js/jpi/nav.js",
-        "assets/js/jpi/main.js"
-    ],
-    admin: [
-        "assets/js/jpi/helpers.js",
-        "assets/js/jpi/stickyFooter.js",
-        "assets/js/third-party/jquery-ui.min.js",
-        "assets/js/third-party/sortable.js"
-    ]
+	main: [
+		"assets/js/jpi/expandedSlideShow.js",
+		"assets/js/jpi/slideShow.js",
+		"assets/js/jpi/helpers.js",
+		"assets/js/jpi/ajax.js",
+		"assets/js/jpi/projects.js",
+		"assets/js/jpi/home.js",
+		"assets/js/jpi/form.js",
+		"assets/js/jpi/stickyFooter.js",
+		"assets/js/third-party/waypoint.min.js",
+		"assets/js/third-party/jquery.countTo.js",
+		"assets/js/jpi/nav.js",
+		"assets/js/jpi/main.js"
+	],
+	admin: [
+		"assets/js/jpi/helpers.js",
+		"assets/js/jpi/stickyFooter.js",
+		"assets/js/third-party/jquery-ui.min.js",
+		"assets/js/third-party/sortable.js"
+	]
 };
 var scriptNames = Object.keys(scripts);
-scriptNames.forEach(function(key, i){
-    gulp.task("scripts-"+key, function() {
-        return gulp.src(scripts[key])
-            .pipe(concat(key+".min.js"))
-            .pipe(uglify())
-            .pipe(gulp.dest("assets/js"));
-    });
+scriptNames.forEach(function (key, i) {
+	gulp.task("scripts-" + key, function () {
+		return gulp.src(scripts[key])
+				.pipe(concat(key + ".min.js"))
+				.pipe(uglify())
+				.pipe(gulp.dest("assets/js"));
+	});
 });
 gulp.task("scripts", ["scripts-main", "scripts-admin"]);
 
 // Minify Stylesheets
 var stylesheets = {
-    main: [
-        "assets/css/style.css"
-    ]
+	main: [
+		"assets/css/style.css"
+	]
 };
 var stylesheetNames = Object.keys(stylesheets);
-stylesheetNames.forEach(function(key, i){
-    gulp.task("styles-"+key, function() {
-        return gulp.src(stylesheets[key])
-            .pipe(concat(key+".min.css"))
-            .pipe(autoprefixer({
-                browsers: ["> 0.5%", "ie 8-11"],
-                remove: false
-            }))
-            .pipe(minifyCss({
-                compatibility: "ie8"
-            }))
-            .pipe(gulp.dest("assets/css"));
-    });
+stylesheetNames.forEach(function (key) {
+	gulp.task("styles-" + key, function () {
+		return gulp.src(stylesheets[key])
+				.pipe(concat(key + ".min.css"))
+				.pipe(autoprefixer({
+					browsers: ["> 0.5%", "ie 8-11"],
+					remove: false
+				}))
+				.pipe(minifyCss({
+					compatibility: "ie8"
+				}))
+				.pipe(gulp.dest("assets/css"));
+	});
 });
 gulp.task("styles", ["styles-main"]);
 
-gulp.task('sass', function() {
-    return gulp.src('assets/css/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css/'));
+gulp.task('sass', function () {
+	return gulp.src('assets/css/style.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('assets/css/'));
 });
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('assets/css/**/*.scss', ['sass', 'styles']);
-    gulp.watch('assets/js/**/*.js', ['scripts']);
+gulp.task('watch', function () {
+	gulp.watch('assets/css/**/*.scss', ['sass', 'styles']);
+	gulp.watch('assets/js/**/*.js', ['scripts']);
 });
 
 gulp.task("default", ["scripts", "styles"]);
