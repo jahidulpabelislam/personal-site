35790534d3a0533ea057eb8dde3747d60570aa53
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-11 17:54:08 +0100

    Rename database.php to Upper cased

--- api/inc/all.php
+++ api/inc/all.php
@@ -8,7 +8,7 @@ date_default_timezone_set("Europe/London");
 
 //using include to include all php files needed
 include $_SERVER['DOCUMENT_ROOT'] . '/config.php';  // Copy config-sample.php and rename to config.php then fill in necessary constant
-include 'database.php';
+include 'Database.php';
 include 'Hasher.php'; // Copy admin/inc/Hasher-sample.php and rename to Hasher.php then update both the functions with your Hashing functionality
 include 'Auth.php';  // Copy admin/inc/Auth-sample.php and rename to Auth.php then update all 3 functions with your Auth functionality
 include 'functions.php';
