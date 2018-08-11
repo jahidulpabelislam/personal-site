ab1354cd22056c8c2a06239439a89f0c7e0d77dd
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-11 17:55:05 +0100

    Rename functions.php to Helper.php

--- api/inc/all.php
+++ api/inc/all.php
@@ -11,5 +11,5 @@ include $_SERVER['DOCUMENT_ROOT'] . '/config.php';  // Copy config-sample.php an
 include 'Database.php';
 include 'Hasher.php'; // Copy admin/inc/Hasher-sample.php and rename to Hasher.php then update both the functions with your Hashing functionality
 include 'Auth.php';  // Copy admin/inc/Auth-sample.php and rename to Auth.php then update all 3 functions with your Auth functionality
-include 'functions.php';
+include 'Helper.php';
 include 'portfolioFunctions.php';
