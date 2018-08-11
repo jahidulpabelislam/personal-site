f8bff4e6b6371f13f763d09eb69ced38088b87cb
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-11 17:55:54 +0100

    Rename portfilioFunction.php to API.php

--- api/inc/all.php
+++ api/inc/all.php
@@ -12,4 +12,4 @@ include 'Database.php';
 include 'Hasher.php'; // Copy admin/inc/Hasher-sample.php and rename to Hasher.php then update both the functions with your Hashing functionality
 include 'Auth.php';  // Copy admin/inc/Auth-sample.php and rename to Auth.php then update all 3 functions with your Auth functionality
 include 'Helper.php';
-include 'portfolioFunctions.php';
+include 'API.php';
