9ee7545bf900583617d04479e8d8cd4ce66d7d85
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-11 15:13:07 +0100

    Remove main hasher file from repo & ignore

--- admin/inc/Hasher.php
+++ admin/inc/Hasher.php
@@ -2,11 +2,24 @@
 
 class Hasher {
 
-	// this will be used to compare a password against a hash
-	public static function checkPassword($password, $hash) {
+	/*
+	 * This will generate a hashed value of given string
+	 */
+	public static function generate($string) {
 
-		$newHash = password_verify($password, $hash);
-		return $hash == $newHash;
+		$options = [
+			'cost' => 11,
+		];
+
+		$hashed = password_hash($string, PASSWORD_BCRYPT, $options);
+
+		return $hashed;
+	}
+
+	// This will be used to compare a raw string against a stored hash
+	public static function check($string, $hash) {
+
+		return password_verify($string, $hash);
 
 	}
 }
