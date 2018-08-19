f371df5c66ba42b8c654f5b510d6ff1fe2a4a803
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-08-19 19:18:06 +0100

    Add custom checkout styling

--- admin/index.php
+++ admin/index.php
@@ -52,9 +52,10 @@
                 <div class="project-select">
 
                     <div class="project-select__form">
-                        <div ng-repeat="project in projects" class="project-select__option">
+                        <div ng-repeat="project in projects" class="project-select__option styled-checkbox">
                             <label ng-click="selectProject(project)" for="{{project.ID}}">{{project.Name}}</label>
-                            <input ng-click="selectProject(project)" type="radio" id="{{project.ID}}" name="project" value="{{project.ID}}">
+                            <input ng-click="selectProject(project)" type="radio" id="{{project.ID}}" name="project" value="{{project.ID}}" class="input">
+	                        <span class="styled-checkbox__pseudo"></span>
                         </div>
                     </div>
 
