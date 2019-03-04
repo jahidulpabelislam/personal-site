a1aa29cdd723f18806573c5c47ebc09c2fc2ba10
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2019-03-04 20:33:20 +0000

    Uppercase Site class filename

--- about/index.php
+++ about/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] . "/site.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/Site.php");
 
 $site = Site::get();
 
--- links/index.php
+++ links/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once($_SERVER["DOCUMENT_ROOT"] . "/site.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/Site.php");
 
 $pageId = "links";
 
