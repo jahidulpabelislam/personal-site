4934a6ae75726e2912ff238198ad027cb0bd10eb
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2019-04-23 23:01:13 +0100

    Tidy up versions

--- PageRenderer.php
+++ PageRenderer.php
@@ -9,7 +9,7 @@
  * PHP version 7
  *
  * @author Jahidul Pabel Islam <me@jahidulpabelislam.com>
- * @version 1
+ * @version 1.0.0
  * @link https://github.com/jahidulpabelislam/portfolio/
  * @since Class available since Release: v4.2.0
  * @copyright 2010-2019 JPI
