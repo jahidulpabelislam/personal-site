fd6c6c560be31e0e8ea70cde4ac3dbbc79026283
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2019-04-23 22:52:04 +0100

    Up Site class version

--- Site.php
+++ Site.php
@@ -8,7 +8,7 @@
  * PHP version 7
  *
  * @author Jahidul Pabel Islam <me@jahidulpabelislam.com>
- * @version 1
+ * @version 1.1
  * @link https://github.com/jahidulpabelislam/portfolio/
  * @since Class available since Release: v4.1.0
  * @copyright 2010-2019 JPI
