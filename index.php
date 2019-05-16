0aaefb5807f4ec51196331ab16219e4470b79a25
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2019-05-16 23:22:29 +0100

    Use real counters from counters json file

--- index.php
+++ index.php
@@ -142,8 +142,11 @@ date_default_timezone_set($orig);
                                 <?php
                                 $speed = 2000;
 
-                                $personalProjectsNum = 24;
-                                $workProjectsNum = 30;
+                                $countersContent = file_get_contents(ROOT . "/assets/counters.json");
+                                $counters = json_decode($countersContent, true);
+
+                                $personalProjectsNum = $counters["personalProjects"] ?? 25;
+                                $workProjectsNum = $counters["workProjects"] ?? 30;
                                 ?>
                                 <p class="article__header article__header--stats counter" data-to="<?php echo $personalProjectsNum; ?>" data-speed="<?php echo $speed; ?>">
                                     <?php echo $personalProjectsNum; ?>
@@ -153,7 +156,7 @@ date_default_timezone_set($orig);
                                 </p>
                             </div>
                             <div class="stats__item">
-                                <?php $totalCommits = 2270; ?>
+                                <?php $totalCommits = $counters["totalCommits"] ?? 8000; ?>
                                 <p class="article__header article__header--stats counter" data-to="<?php echo $totalCommits; ?>" data-speed="<?php echo $speed; ?>">
                                     <?php echo $totalCommits; ?>
                                 </p>
