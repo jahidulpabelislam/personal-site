341dcb1c20a44931c76873227cd0d3d769703f62
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-08-20 21:03:44 +0100

    Allow referer on external validators

--- partials/footer.php
+++ partials/footer.php
@@ -63,8 +63,8 @@ $pageRenderer = PageRenderer::get();
                     <p>
                         <a class="footer__link" href="<?php $site->echoURL("site-map"); ?>">Site Map</a>
                         <a class="footer__link" href="<?php $site->echoURL("privacy-policy"); ?>">Privacy Policy</a>
-                        <a class="footer__link" href="https://validator.w3.org/check/?uri=referer" target="_blank" rel="noopener noreferrer">Valid HTML</a>
-                        <a class="footer__link" href="https://jigsaw.w3.org/css-validator/check/referer/" target="_blank" rel="noopener noreferrer">Valid CSS</a>
+                        <a class="footer__link" href="https://validator.w3.org/check/?uri=referer" target="_blank" rel="noopener">Valid HTML</a>
+                        <a class="footer__link" href="https://jigsaw.w3.org/css-validator/check/referer/" target="_blank" rel="noopener">Valid CSS</a>
                     </p>
                 </div>
 
