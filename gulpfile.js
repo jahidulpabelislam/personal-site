44c95665646c574543b62521a151eeb2185895a1
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-09-03 22:22:47 +0100

    Better variable name for Url

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ const runCommand = function(command, callback) {
 
 defaultTasks.push("store-version");
 gulp.task("store-version", function() {
-    const githubBaseUrl = "https://github.com/jahidulpabelislam/portfolio";
+    const githubBaseURL = "https://github.com/jahidulpabelislam/portfolio";
     const fileName = `${assetsDir}/version.txt`;
     let versionText = "";
 
@@ -123,7 +123,7 @@ gulp.task("store-version", function() {
          * Else if production, find and get the current running tag to use as the current 'version'
          */
         if (branchName && branchName !== "master" && !branchName.startsWith("(HEAD detached at v")) {
-            versionText = `<a href="${githubBaseUrl}/tree/${branchName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${branchName}</a>`;
+            versionText = `<a href="${githubBaseURL}/tree/${branchName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${branchName}</a>`;
             return fs.writeFile(fileName, versionText, errorCallback);
         }
 
@@ -131,11 +131,11 @@ gulp.task("store-version", function() {
         return runCommand("git describe --abbrev=0 --tags", function(tagName) {
             // If found store in text file
             if (tagName) {
-                versionText = `<a href="${githubBaseUrl}/releases/tag/${tagName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${tagName}</a>`;
+                versionText = `<a href="${githubBaseURL}/releases/tag/${tagName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${tagName}</a>`;
             }
             // Else drop back to branch name if exists else remove version value from file
             else if (branchName) {
-                versionText = `<a href="${githubBaseUrl}/tree/${branchName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${branchName}</a>`;
+                versionText = `<a href="${githubBaseURL}/tree/${branchName}/" class="link-styled" target="_blank" rel="noopener noreferrer">${branchName}</a>`;
             }
 
             fs.writeFile(fileName, versionText, errorCallback);
