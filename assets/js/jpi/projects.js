c9f3316c011e73e433bc60db6693edddd7306ffc
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-09-28 00:03:44 +0100

    Update the layout on project cards

--- assets/js/jpi/projects.js
+++ assets/js/jpi/projects.js
@@ -328,6 +328,7 @@ window.jpi.projects = (function(jQuery, jpi) {
                 query = {
                     page: page,
                     search: search,
+                    limit: 6,
                 };
 
             // Stops all the slide shows
