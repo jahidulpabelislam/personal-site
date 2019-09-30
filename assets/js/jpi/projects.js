94dce7c93f757b9cb7bdbf34d906957c77063ad6
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-09-30 22:17:45 +0100

    Add project type to project modal (& minor restyle)

--- assets/js/jpi/projects.js
+++ assets/js/jpi/projects.js
@@ -229,9 +229,17 @@ window.jpi.projects = (function(jQuery, jpi) {
 
             modal.find(".project__links, .project__skills, .slide-show__slides-container, .slide-show__bullets").text("");
 
-            modal.find(".project__title").text(project.name);
+            modal.find(".modal__heading").text(project.name);
             modal.find(".project__date").text(project.date);
             modal.find(".project__description").html(project.long_description);
+
+            var projectTypeElem = modal.find(".project__type");
+
+            projectTypeElem.text(project.type);
+
+            var classList = projectTypeElem.attr("class");
+            classList = classList.replace(global.typeColourRegex, "project__type--" + project.colour);
+            projectTypeElem.attr("class", classList);
 
             fn.addSkills(project, global.modalSelector);
             fn.addLinks(project, global.modalSelector);
@@ -468,6 +476,7 @@ window.jpi.projects = (function(jQuery, jpi) {
         init: function() {
             if (jQuery(".js-all-projects").length) {
                 global.dateFormat = new Intl.DateTimeFormat(undefined, {month: "long", year: "numeric"});
+                global.typeColourRegex = new RegExp("project__type--[\\w-]*", "g");
 
                 fn.initListeners();
                 fn.getProjects();
