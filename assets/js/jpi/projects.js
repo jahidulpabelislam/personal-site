bb4859b2c1c5a47ab7b5fd8709d3e738f17be1db
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-09-30 19:12:35 +0100

    Only format project date once

--- assets/js/jpi/projects.js
+++ assets/js/jpi/projects.js
@@ -184,6 +184,11 @@ window.jpi.projects = (function(jQuery, jpi) {
                 return;
             }
 
+            if (project.date) {
+                var date = new Date(project.date);
+                project.date = global.dateFormat.format(date);
+            }
+
             // Make sure colour placeholders are replaced in content
             var colourRegex = fn.getTemplateRegex("colour");
             var fields = ["short_description", "long_description"];
@@ -198,13 +203,7 @@ window.jpi.projects = (function(jQuery, jpi) {
             for (var field in project) {
                 if (project.hasOwnProperty(field) && typeof field === "string") {
                     var regex = fn.getTemplateRegex(field);
-
                     var value = project[field];
-                    if (field === "date") {
-                        var date = new Date(value);
-                        value = global.dateFormat.format(date);
-                    }
-
                     template = template.replace(regex, value);
                 }
             }
@@ -231,9 +230,7 @@ window.jpi.projects = (function(jQuery, jpi) {
             modal.find(".project__links, .project__skills, .slide-show__slides-container, .slide-show__bullets").text("");
 
             modal.find(".project__title").text(project.name);
-
-            var projectDateString = global.dateFormat.format(new Date(project.date));
-            modal.find(".project__date").text(projectDateString);
+            modal.find(".project__date").text(project.date);
             modal.find(".project__description").html(project.long_description);
 
             fn.addSkills(project, global.modalSelector);
