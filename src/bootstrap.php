5e581f9c0e692354f2b68a5fae0588bdd5aca127
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-11-30 14:22:52 +0000

    Move config stuff from bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,13 +2,7 @@
 
 include_once("functions.php");
 
-if (!defined("JPI_DATE_TIMEZONE")) {
-    define("JPI_DATE_TIMEZONE", "Europe/London");
-}
-
-if (!defined("ROOT")) {
-    define("ROOT", getProjectRoot());
-}
+include_once("config.php");
 
 include_once("Site.php");
 include_once("Page.php");
---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined("JPI_DATE_TIMEZONE")) {
+    define("JPI_DATE_TIMEZONE", "Europe/London");
+}
+
+if (!defined("ROOT")) {
+    define("ROOT", getProjectRoot());
+}
