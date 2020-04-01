56448bf265b158e1fdeaad0227bb713cf6df5aea
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-04-02 00:53:25 +0100

    Fix indentation

--- links/index.php
+++ links/index.php
@@ -15,58 +15,58 @@ $page->addPageData($pageData);
 $page->renderHTMLHead();
 ?>
 
-        <main class="main-content social-links-page">
-            <div class="container">
-                <div class="social-link-container">
-                    <a class="social-link" href="https://facebook.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                        <img class="social-link__image social-link__image--facebook" src="<?php echoWithAssetVersion("/assets/images/logos/facebook.svg"); ?>" alt="Add Me facebook.com/jahidulpabelislam" />
-                        <p class="social-link__text social-link__text--facebook">/jahidulpabelislam</p>
-                    </a>
-                </div>
+            <main class="main-content social-links-page">
+                <div class="container">
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://facebook.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                            <img class="social-link__image social-link__image--facebook" src="<?php echoWithAssetVersion("/assets/images/logos/facebook.svg"); ?>" alt="Add Me facebook.com/jahidulpabelislam" />
+                            <p class="social-link__text social-link__text--facebook">/jahidulpabelislam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container">
-                    <a class="social-link" href="https://twitter.com/itsjahidulislam/" target="_blank" rel="noopener noreferrer">
-                        <img class="social-link__image social-link__image--twitter" src="<?php echoWithAssetVersion("/assets/images/logos/twitter.png"); ?>" alt="Follow Me @ItsJahidulIslam" />
-                        <p class="social-link__text social-link__text--twitter">@ItsJahidulIslam</p>
-                    </a>
-                </div>
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://twitter.com/itsjahidulislam/" target="_blank" rel="noopener noreferrer">
+                            <img class="social-link__image social-link__image--twitter" src="<?php echoWithAssetVersion("/assets/images/logos/twitter.png"); ?>" alt="Follow Me @ItsJahidulIslam" />
+                            <p class="social-link__text social-link__text--twitter">@ItsJahidulIslam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container">
-                    <a class="social-link" href="https://instagram.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                        <span class="social-link__image social-link__image--instagram"><i></i></span>
-                        <p class="social-link__text social-link__text--instagram">@jahidulpabelislam</p>
-                    </a>
-                </div>
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://instagram.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                            <span class="social-link__image social-link__image--instagram"><i></i></span>
+                            <p class="social-link__text social-link__text--instagram">@jahidulpabelislam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container social-link-container--snapchat">
-                    <a class="social-link" href="https://snapchat.com/add/jahidulpislam/" target="_blank" rel="noopener noreferrer">
-                        <object class="social-link__image social-link__image--snapchat" data="https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=jahidulpislam&amp;type=PNG" type="image/png"></object>
-                        <p class="social-link__text social-link__text--snapchat">jahidulpislam</p>
-                    </a>
-                </div>
+                    <div class="social-link-container social-link-container--snapchat">
+                        <a class="social-link" href="https://snapchat.com/add/jahidulpislam/" target="_blank" rel="noopener noreferrer">
+                            <object class="social-link__image social-link__image--snapchat" data="https://feelinsonice-hrd.appspot.com/web/deeplink/snapcode?username=jahidulpislam&amp;type=PNG" type="image/png"></object>
+                            <p class="social-link__text social-link__text--snapchat">jahidulpislam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container">
-                    <a class="social-link" href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                        <img class="social-link__image social-link__image--linkedin" src="<?php echoWithAssetVersion("/assets/images/logos/linkedin.svg"); ?>" alt="Find me on LinkedIn /jahidulpabelislam" />
-                        <p class="social-link__text social-link__text--linkedin">/jahidulpabelislam</p>
-                    </a>
-                </div>
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                            <img class="social-link__image social-link__image--linkedin" src="<?php echoWithAssetVersion("/assets/images/logos/linkedin.svg"); ?>" alt="Find me on LinkedIn /jahidulpabelislam" />
+                            <p class="social-link__text social-link__text--linkedin">/jahidulpabelislam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container">
-                    <a class="social-link" href="https://github.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                        <img class="social-link__image social-link__image--github" src="<?php echoWithAssetVersion("/assets/images/logos/github.svg"); ?>" alt="Find me on GitHub /jahidulpabelislam" />
-                        <p class="social-link__text social-link__text--github">/jahidulpabelislam</p>
-                    </a>
-                </div>
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://github.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                            <img class="social-link__image social-link__image--github" src="<?php echoWithAssetVersion("/assets/images/logos/github.svg"); ?>" alt="Find me on GitHub /jahidulpabelislam" />
+                            <p class="social-link__text social-link__text--github">/jahidulpabelislam</p>
+                        </a>
+                    </div>
 
-                <div class="social-link-container">
-                    <a class="social-link" href="https://instagram.com/jpi.dev/" target="_blank" rel="noopener noreferrer">
-                        <span class="social-link__image social-link__image--instagram"><i></i></span>
-                        <p class="social-link__text social-link__text--instagram">@jpi.dev</p>
-                    </a>
+                    <div class="social-link-container">
+                        <a class="social-link" href="https://instagram.com/jpi.dev/" target="_blank" rel="noopener noreferrer">
+                            <span class="social-link__image social-link__image--instagram"><i></i></span>
+                            <p class="social-link__text social-link__text--instagram">@jpi.dev</p>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
         </div>
 
         <?php
--- partials/footer.php
+++ partials/footer.php
@@ -7,82 +7,82 @@ $site = Site::get();
 $page = Page::get();
 ?>
 
-                <?php
-                if (count($similarLinks)) {
-                    $linksContent = "";
-                    foreach ($similarLinks as $link) {
-                        $pageTitle = $link["title"];
-                        $buttonText = $link["text"] ?? $pageTitle;
+                    <?php
+                    if (count($similarLinks)) {
+                        $linksContent = "";
+                        foreach ($similarLinks as $link) {
+                            $pageTitle = $link["title"];
+                            $buttonText = $link["text"] ?? $pageTitle;
 
-                        $url = $link["url"];
-                        $url = $site->getURL($url);
+                            $url = $link["url"];
+                            $url = $site->getURL($url);
 
-                        $buttonClasses = ["button"];
+                            $buttonClasses = ["button"];
 
-                        if (!empty($link["colour"])){
-                            $buttonClasses[] =  "button--{$link["colour"]}";
+                            if (!empty($link["colour"])){
+                                $buttonClasses[] =  "button--{$link["colour"]}";
+                            }
+
+                            $buttonClass = implode(" ", $buttonClasses);
+
+                            $linksContent .= <<<HTML
+                                <div class="row__column">
+                                    <a class="{$buttonClass}" href="{$url}" title="Link to {$pageTitle} Page">
+                                        {$buttonText}
+                                    </a>
+                                </div>
+                                HTML;
                         }
 
-                        $buttonClass = implode(" ", $buttonClasses);
-
-                        $linksContent .= <<<HTML
-                            <div class="row__column">
-                                <a class="{$buttonClass}" href="{$url}" title="Link to {$pageTitle} Page">
-                                    {$buttonText}
-                                </a>
+                        echo <<<HTML
+                            <div class="row row--split similar-links">
+                                <div class="container">
+                                    {$linksContent}
+                                </div>
                             </div>
                             HTML;
                     }
+                    ?>
 
-                    echo <<<HTML
-                        <div class="row row--split similar-links">
-                            <div class="container">
-                                {$linksContent}
-                            </div>
+                    <!-- End dynamic content -->
+                    <section class="social-links">
+                        <div class="container">
+                            <h1 class="social-links__heading">Follow Me Here!</h1>
+                            <a class="social-link" href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                                <img class="social-links__image social-link__image social-link__image--linkedin" src="<?php echoWithAssetVersion("/assets/images/logos/linkedin.svg"); ?>" alt="Find me on LinkedIn /jahidulpabelislam" />
+                            </a>
+                            <a class="social-link" href="https://github.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
+                                <img class="social-links__image social-link__image social-link__image--github" src="<?php echoWithAssetVersion("/assets/images/logos/github.svg"); ?>" alt="Find me on GitHub /jahidulpabelislam" />
+                            </a>
+                            <a class="social-link" href="https://www.instagram.com/jpi.dev/" target="_blank" rel="noopener noreferrer">
+                                <span class="social-links__image social-link__image social-link__image--instagram"><i></i></span>
+                            </a>
                         </div>
-                        HTML;
-                }
-                ?>
+                    </section>
+                </div>
+            </main>
 
-                <!-- End dynamic content -->
-                <section class="social-links">
-                    <div class="container">
-                        <h1 class="social-links__heading">Follow Me Here!</h1>
-                        <a class="social-link" href="https://uk.linkedin.com/in/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                            <img class="social-links__image social-link__image social-link__image--linkedin" src="<?php echoWithAssetVersion("/assets/images/logos/linkedin.svg"); ?>" alt="Find me on LinkedIn /jahidulpabelislam" />
-                        </a>
-                        <a class="social-link" href="https://github.com/jahidulpabelislam/" target="_blank" rel="noopener noreferrer">
-                            <img class="social-links__image social-link__image social-link__image--github" src="<?php echoWithAssetVersion("/assets/images/logos/github.svg"); ?>" alt="Find me on GitHub /jahidulpabelislam" />
-                        </a>
-                        <a class="social-link" href="https://www.instagram.com/jpi.dev/" target="_blank" rel="noopener noreferrer">
-                            <span class="social-links__image social-link__image social-link__image--instagram"><i></i></span>
-                        </a>
+            <!-- Footer for site -->
+            <footer class="footer">
+                <div class="container">
+                    <div class="footer__version">
+                        <p><?php renderFile("/assets/version.txt"); ?></p>
                     </div>
-                </section>
-            </div>
-        </main>
 
-        <!-- Footer for site -->
-        <footer class="footer">
-            <div class="container">
-                <div class="footer__version">
-                    <p><?php renderFile("/assets/version.txt"); ?></p>
-                </div>
+                    <div class="footer__links">
+                        <p>
+                            <a class="footer__link" href="<?php $site->echoURL("site-map"); ?>">Site Map</a>
+                            <a class="footer__link" href="<?php $site->echoURL("privacy-policy"); ?>">Privacy Policy</a>
+                            <a class="footer__link" href="https://validator.w3.org/check/?uri=referer" target="_blank" rel="noopener">Valid HTML</a>
+                            <a class="footer__link" href="https://jigsaw.w3.org/css-validator/check/referer/" target="_blank" rel="noopener">Valid CSS</a>
+                        </p>
+                    </div>
 
-                <div class="footer__links">
-                    <p>
-                        <a class="footer__link" href="<?php $site->echoURL("site-map"); ?>">Site Map</a>
-                        <a class="footer__link" href="<?php $site->echoURL("privacy-policy"); ?>">Privacy Policy</a>
-                        <a class="footer__link" href="https://validator.w3.org/check/?uri=referer" target="_blank" rel="noopener">Valid HTML</a>
-                        <a class="footer__link" href="https://jigsaw.w3.org/css-validator/check/referer/" target="_blank" rel="noopener">Valid CSS</a>
-                    </p>
+                    <div class="footer__legal">
+                        <p>&copy; Jahidul Pabel Islam <?php echo $site->getYearStarted() . " - " . date("Y"); ?></p>
+                    </div>
                 </div>
-
-                <div class="footer__legal">
-                    <p>&copy; Jahidul Pabel Islam <?php echo $site->getYearStarted() . " - " . date("Y"); ?></p>
-                </div>
-            </div>
-        </footer>
+            </footer>
 
             <?php
             $page->renderCookieBanner();
