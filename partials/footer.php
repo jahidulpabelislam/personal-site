88ca91079970b17d7777ee9964eb9a6a512dc1e5
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-03-24 23:36:35 +0000

    Remove the 2 validator links in footer

--- partials/footer.php
+++ partials/footer.php
@@ -16,8 +16,6 @@ $site = Site::get();
             <p>
                 <a class="footer__link" href="<?php $site->echoURL("site-map"); ?>">Site Map</a>
                 <a class="footer__link" href="<?php $site->echoURL("privacy-policy"); ?>">Privacy Policy</a>
-                <a class="footer__link" href="https://validator.w3.org/check/?uri=referer" target="_blank" rel="noopener">Valid HTML</a>
-                <a class="footer__link" href="https://jigsaw.w3.org/css-validator/check/referer/" target="_blank" rel="noopener">Valid CSS</a>
             </p>
         </div>
 
