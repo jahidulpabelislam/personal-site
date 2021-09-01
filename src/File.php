1fc25f34d6c5e14324768ceebb6aa1c41963ef64
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2021-09-01 23:28:10 +0100

    Separate path & full path & add getter

--- src/File.php
+++ src/File.php
@@ -15,22 +15,29 @@ namespace App;
 class File {
 
     private $path;
+    private $fullPath;
 
     private $exists = null;
     private $contents = null;
     private $contentsAsArray = null;
 
     public function __construct(string $path, bool $isRelative = true) {
+        $this->path = $path;
+
         if ($isRelative) {
             $path = PUBLIC_ROOT . Site::addTrailingSlash($path);
         }
 
-        $this->path = $path;
+        $this->fullPath = $path;
+    }
+
+    public function getPath(): string {
+        return $this->path;
     }
 
     public function exists(): bool {
         if ($this->exists === null) {
-            $this->exists = file_exists($this->path);
+            $this->exists = file_exists($this->fullPath);
         }
 
         return $this->exists;
@@ -38,13 +45,13 @@ class File {
 
     public function include(): void {
         if ($this->exists()) {
-            include_once($this->path);
+            include_once($this->fullPath);
         }
     }
 
     public function get($default = null) {
         if ($this->contents === null && $this->exists()) {
-            $this->contents = file_get_contents($this->path);
+            $this->contents = file_get_contents($this->fullPath);
         }
 
         return $this->contents ?? $default;
@@ -65,5 +72,4 @@ class File {
     public function render(string $default = ""): void {
         echo $this->get($default);
     }
-
 }
