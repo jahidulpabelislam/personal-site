ff39861aeeec3a7860aa24f67c38ed8da1f80569
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2021-10-03 13:56:25 +0100

    Restyle mobile nav

--- dev/js/jpi/components/nav.js
+++ dev/js/jpi/components/nav.js
@@ -5,21 +5,10 @@
     var $window = jQuery(window);
 
     this.$element = jQuery(".nav");
-    this.$menuButton = jQuery(".nav__mobile-toggle");
-    this.$linksContainers = jQuery(".nav__links-container");
 
     var $header = jQuery(".header");
 
-    this.toggleMobileMenu = function() {
-        this.$element.toggleClass("nav--open");
-        this.$linksContainers.slideToggle();
-    };
-
     this.reset = function() {
-        if (window.innerWidth >= JPI.getInt(JPI.breakpoints.tablet)) {
-            this.$linksContainers.css("display", "");
-        }
-
         // Set the correct class on nav depending on current scroll position
         var navHeight = this.$element.height();
         var scrollPos = $window.scrollTop() + navHeight;
@@ -33,23 +22,10 @@
         }
     };
 
-    // Code to collapse mobile menu when user clicks anywhere off it.
-    this.onNavClick = function(e) {
-        if (
-            this.$element.hasClass("nav--open") &&
-            !jQuery(e.target).closest(".nav").length &&
-            this.$menuButton.css("display") !== "none"
-        ) {
-            this.$menuButton.click();
-        }
-    };
-
     this.init = function() {
         this.reset();
 
-        $window.on("click", this.onNavClick.bind(this));
         $window.on("scroll orientationchange resize", JPI.debounce(this.reset.bind(this), 150));
-        this.$menuButton.on("click", this.toggleMobileMenu.bind(this));
     };
 
     $window.on("jpi-css-loaded", this.init.bind(this));
--- partials/nav.php
+++ partials/nav.php
@@ -8,49 +8,39 @@ $currentURL = $page->currentURL;
 
 <nav class="nav">
     <div class="container nav__container">
-        <div class="nav__header">
-            <button type="button" class="nav__mobile-toggle">
-                <span class="screen-reader-text">Toggle navigation</span>
-                <span class="nav__menu-bar"></span>
-                <span class="nav__menu-bar"></span>
-                <span class="nav__menu-bar"></span>
-            </button>
-            <a class="nav__logo <?php if ($pageId === "home") {echo "nav__logo--active";} ?>" href="<?php echo $site->makeURL(); ?>">
-                <img src="<?php echo $site::asset("/assets/images/logo-v2.png"); ?>" alt="<?php echo $site::NAME; ?>'s Logo" />
-            </a>
-        </div>
-        <div class="nav__links-container">
-            <ul class="nav__links">
-                <?php
-                $links = [
-                    [
-                        "title" => "My Projects",
-                        "url" => "/projects/",
-                    ],
-                ];
+        <a class="nav__logo <?php if ($pageId === "home") {echo "nav__logo--active";} ?>" href="<?php echo $site->makeURL(); ?>">
+            <img src="<?php echo $site::asset("/assets/images/logo-v2.png"); ?>" alt="<?php echo $site::NAME; ?>'s Logo" />
+        </a>
+        <ul class="nav__links">
+            <?php
+            $links = [
+                [
+                    "title" => "My Projects",
+                    "url" => "/projects/",
+                ],
+            ];
 
-                foreach ($links as $link) {
-                    $linkTitle = $link["title"];
+            foreach ($links as $link) {
+                $linkTitle = $link["title"];
 
-                    $url = $link["url"] ?? "/";
-                    $fullURL = $site->makeURL($url);
+                $url = $link["url"] ?? "/";
+                $fullURL = $site->makeURL($url);
 
-                    $classes = ["nav__link"];
-                    if ($currentURL === $site->makeURL($url, false)) {
-                        $classes[] = "nav__link--active";
-                    }
-
-                    $class = implode(" ", $classes);
-                    echo <<<HTML
-                        <li class="nav__item">
-                            <a class="{$class}" href="{$fullURL}" title="Link to {$linkTitle} Page">
-                                {$linkTitle}
-                            </a>
-                        </li>
-                        HTML;
+                $classes = ["nav__link"];
+                if ($currentURL === $site->makeURL($url, false)) {
+                    $classes[] = "nav__link--active";
                 }
-                ?>
-            </ul>
-        </div>
+
+                $class = implode(" ", $classes);
+                echo <<<HTML
+                    <li class="nav__item">
+                        <a class="$class" href="$fullURL" title="Link to $linkTitle Page">
+                            $linkTitle
+                        </a>
+                    </li>
+                    HTML;
+            }
+            ?>
+        </ul>
     </div>
 </nav>
