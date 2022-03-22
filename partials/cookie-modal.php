677667b644d09daad30f4a41196f04f2a22af505
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-03-22 23:44:54 +0000

    Split cookies bits from privacy policy

--- partials/cookie-modal.php
+++ partials/cookie-modal.php
@@ -3,7 +3,7 @@
         <div class="cookie-modal__text">
             <p>
                 This website uses cookies via Google Analytics, by viewing the website you agree to use cookies. Learn more about this
-                <a class="link cookie-modal__link" href="<?php echo site()->makeURL("/privacy-policy/"); ?>">here</a>.
+                <a class="link cookie-modal__link" href="<?php echo site()->makeURL("/cookie-policy/"); ?>">here</a>.
             </p>
         </div>
         <div class="cookie-modal__actions">
---
+++ public/cookie-policy/index.php
@@ -0,0 +1,84 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"] . "/../bootstrap.php");
+
+$site = site();
+$page = page();
+
+$name = $site::NAME;
+$job = $site::JOB;
+
+$page->renderHtmlStart();
+$page->renderHead([
+    "title" => "Cookie Policy",
+    "hdescription" => "Cookie policy for the site of $name, a $job based at Bognor Regis, West Sussex down by the South Coast of England.",
+]);
+$page->renderBodyStart();
+$page->renderPageStart();
+$page->renderNav();
+$page->renderHeader([
+    "title" => "Cookie Policy",
+]);
+$page->renderContentStart();
+?>
+
+<div class="row row--alt">
+    <div class="container">
+        <h3 class="row__heading">Cookies</h3>
+        <p>This website uses cookies to collect information. Cookies are files stored on your device when you come on a website which is using them. The data stored can be a whole range of data, user login session data to shopping basket.</p>
+        <p>Cookies on this site is used to allow tracking user behaviour with the site, to use in analytics and improving the site.</p>
+        <p>By viewing the site, it will be assumed you agree to use cookies within this site.</p>
+
+        <h4 class="row__sub-heading">Cookies used</h4>
+        <p><strong>Google Analytics:</strong></p>
+        <table class="table">
+            <tr class="table__row table__row--header">
+                <th class="table__col table__col--header">Cookie Name</th>
+                <th class="table__col table__col--header">Expiration Time</th>
+                <th class="table__col table__col--header">Description</th>
+            </tr>
+            <tr class="table__row">
+                <td class="table__col">_ga</td>
+                <td class="table__col">2 years</td>
+                <td class="table__col">Used to distinguish unique users.</td>
+            </tr>
+            <tr class="table__row">
+                <td class="table__col">_gat</td>
+                <td class="table__col">10 minutes</td>
+                <td class="table__col">Used to throttle request rate.</td>
+            </tr>
+            <tr class="table__row">
+                <td class="table__col">cookie-modal-closed</td>
+                <td class="table__col">30 days</td>
+                <td class="table__col">Used to make sure the modal isn't shown if clicked 'OK'.</td>
+            </tr>
+        </table>
+        <p>The cookies used and the Google Analytics data stored will be anonymised and can't be used to identify individual people.</p>
+        <p>
+            To stop cookies you will need to go to your browser settings, for more information on this as well as general/more information on cookies can be found at
+            <a class="link" href="https://www.allaboutcookies.org/" target="_blank" rel="noopener noreferrer">allaboutcookies</a>
+        </p>
+    </div>
+</div>
+
+<?php
+$page->renderSimilarLinks([
+    "links" => [
+        [
+            "title" => "Portfolio",
+            "url" => "/portfolio/",
+            "text" => "View My Work",
+        ],
+        [
+            "title" => "Contact",
+            "url" => "/contact/",
+            "text" => "Get in Touch",
+        ],
+    ],
+]);
+$page->renderSocialLinks();
+$page->renderContentEnd();
+$page->renderFooter();
+$page->renderPageEnd();
+$page->renderCookieModal();
+$page->renderBodyEnd();
+$page->renderHtmlEnd();
