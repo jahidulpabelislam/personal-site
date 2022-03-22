c2db6a5c496560324faa2e3c23661f43f247eacd
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-03-22 23:59:04 +0000

    Update cookies page contents

--- public/cookie-policy/index.php
+++ public/cookie-policy/index.php
@@ -27,35 +27,45 @@ $page->renderContentStart();
         <p>This website uses cookies to collect information. Cookies are files stored on your device when you come on a website which is using them. The data stored can be a whole range of data, user login session data to shopping basket.</p>
         <p>Cookies on this site is used to allow tracking user behaviour with the site, to use in analytics and improving the site.</p>
         <p>By viewing the site, it will be assumed you agree to use cookies within this site.</p>
-
-        <h4 class="row__sub-heading">Cookies used</h4>
-        <p><strong>Google Analytics:</strong></p>
+        <h4 class="row__sub-heading">Cookies used:</h4>
         <table class="table">
             <tr class="table__row table__row--header">
                 <th class="table__col table__col--header">Cookie Name</th>
+                <th class="table__col table__col--header">Category</th>
+                <th class="table__col table__col--header">Type</th>
+                <th class="table__col table__col--header">Domain</th>
                 <th class="table__col table__col--header">Expiration Time</th>
                 <th class="table__col table__col--header">Description</th>
             </tr>
             <tr class="table__row">
                 <td class="table__col">_ga</td>
+                <td class="table__col">Performance </td>
+                <td class="table__col">First Party</td>
+                <td class="table__col">.jahidulpabelislam.com</td>
                 <td class="table__col">2 years</td>
-                <td class="table__col">Used to distinguish unique users.</td>
+                <td class="table__col">Used to distinguish unique users</td>
             </tr>
             <tr class="table__row">
                 <td class="table__col">_gat</td>
+                <td class="table__col">todo</td>
+                <td class="table__col">First Party</td>
+                <td class="table__col">.jahidulpabelislam.com</td>
                 <td class="table__col">10 minutes</td>
-                <td class="table__col">Used to throttle request rate.</td>
+                <td class="table__col">Used to throttle request rate</td>
             </tr>
             <tr class="table__row">
                 <td class="table__col">cookie-modal-closed</td>
+                <td class="table__col">Strictly Necessary</td>
+                <td class="table__col">First Party</td>
+                <td class="table__col">jahidulpabelislam.com</td>
                 <td class="table__col">30 days</td>
-                <td class="table__col">Used to make sure the modal isn't shown if clicked 'OK'.</td>
+                <td class="table__col">Used to make sure the modal isn't shown if closed already</td>
             </tr>
         </table>
         <p>The cookies used and the Google Analytics data stored will be anonymised and can't be used to identify individual people.</p>
         <p>
             To stop cookies you will need to go to your browser settings, for more information on this as well as general/more information on cookies can be found at
-            <a class="link" href="https://www.allaboutcookies.org/" target="_blank" rel="noopener noreferrer">allaboutcookies</a>
+            <a class="link" href="https://www.allaboutcookies.org/" target="_blank" rel="noopener noreferrer">allaboutcookies</a>.
         </p>
     </div>
 </div>
