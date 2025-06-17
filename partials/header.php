5e406affefda7b046dc4c1cd17fa9fa353975018
Jahidul Pabel Islam <jahidul@d3r.com>
2025-06-17 23:08:01 +0100

    Hide desc if none

--- partials/header.php
+++ partials/header.php
@@ -7,8 +7,10 @@ $page = page();
     <div class="header__overlay">
         <div class="container">
             <h1 class="header__title"><?php echo $this->title; ?></h1>
-            <hr class="header__line-breaker" />
-            <h2 class="header__description"><?php echo $this->description ?? ""; ?></h2>
+            <?php if (!empty($this->description)): ?>
+                <hr class="header__line-breaker" />
+                <h2 class="header__description"><?php echo $this->description; ?></h2>
+            <?php endif; ?>
         </div>
     </div>
 </header>
