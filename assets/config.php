58fa135157530f12e6c70538ada08e1811a3becd
Jahidul Pabel Islam <jahidul@d3r.com>
2025-08-01 23:00:49 +0100

    Update domains for dev

--- assets/config.php
+++ assets/config.php
@@ -5,24 +5,24 @@ const JPI_API_VERSION = "4";
 $environment = site()->getEnvironment();
 if ($environment === "development") {
     if (!defined("LINK_TO_URL")) {
-        define("LINK_TO_URL", "http://linkto.local");
+        define("LINK_TO_URL", "http://linkto.jahidulpabelislam.local");
     }
 
     if (!defined("LINKS_URL")) {
-        define("LINKS_URL", "http://links.local");
+        define("LINKS_URL", "http://links.jahidulpabelislam.local");
     }
 
     if (!defined("JPI_API_ENDPOINT")) {
-        define("JPI_API_ENDPOINT", "http://portfolio-api.local");
+        define("JPI_API_ENDPOINT", "http://api.jahidulpabelislam.local");
     }
 }
 else if ($environment === "staging") {
     if (!defined("LINK_TO_URL")) {
-        define("LINK_TO_URL", "https://staging.linkto.jahidulpabelislam.com/");
+        define("LINK_TO_URL", "https://staging.linkto.jahidulpabelislam.com");
     }
 
     if (!defined("LINKS_URL")) {
-        define("LINKS_URL", "https://staging.links.jahidulpabelislam.com/");
+        define("LINKS_URL", "https://staging.links.jahidulpabelislam.com");
     }
 
     if (!defined("JPI_API_ENDPOINT")) {
@@ -30,11 +30,11 @@ else if ($environment === "staging") {
     }
 } else {
     if (!defined("LINK_TO_URL")) {
-        define("LINK_TO_URL", "https://linkto.jahidulpabelislam.com/");
+        define("LINK_TO_URL", "https://linkto.jahidulpabelislam.com");
     }
 
     if (!defined("LINKS_URL")) {
-        define("LINKS_URL", "https://links.jahidulpabelislam.com/");
+        define("LINKS_URL", "https://links.jahidulpabelislam.com");
     }
 
     if (!defined("JPI_API_ENDPOINT")) {
